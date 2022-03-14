e1e1d2d97c616ad4c52a2e030f5284f50a3bf875
Felipe Del Valle <fadelvalle@uc.cl>
2022-03-14 07:53:02 +0200

    Update binder_MFI_2ChannelsRatio.R
    Updated version

--- binder_MFI_2ChannelsRatio.R
+++ binder_MFI_2ChannelsRatio.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(tidyverse)
 library(ggplot2)
+library(sjmisc)
+library(ggstatsplot)
 #import list of files as .csv pattern recognition and create a list of files 
 file.list <- list.files(pattern='*.csv')
 filelist <- as.data.frame(file.list)
@@ -10,12 +12,11 @@ df <- bind_rows(df.list, .id = "id")
 df_data <- as.data.frame(df)
 df_data <- df_data %>% rename("Imgnum" = ...1)
 
-#Specify the channels you need , in this case only 2 and 4 are selected
+
 grouped_by_chn<-df_data %>% group_by(Ch)
 grouped_by_chn<- grouped_by_chn %>% filter(Ch==2|Ch==4)
 grouped_by_chn<-as.data.frame(grouped_by_chn)
 
-#create 2 dataframes for each channel, might be usefull to look at them individually 
 chn2 <- grouped_by_chn %>% filter(Ch==2)
 chn4 <- grouped_by_chn %>% filter(Ch==4)
 
@@ -26,9 +27,19 @@ ratio_merge$chn2<-chn2$Mean
 ratio_merge$chn4<-chn4$Mean
 ratio_merge <- ratio_merge %>% mutate(ratio = chn2/chn4)
 
-xlsx::write.xlsx(x = grouped_by_chn, file = "resultados.xls", sheetName = "res", col.names = TRUE , row.names = FALSE, append = TRUE)
+ratiomerge_container<-ratio_merge%>% mutate(Status = case_when(
+  
+  grepl("12kPa_15min", id) ~ "12kpa_15min",
+  grepl("12kPa_30min", id) ~ "12kpa_30min",
+  grepl("05kPa_15min", id) ~ "05kpa_15min",
+  grepl("05kPa_5min", id) ~ "05kpa_5min",
+  grepl("12kPa_5min", id) ~ "12kpa_5min",
+  grepl("05kPa_30min", id) ~ "05kpa_30min"
+))
 
-xlsx::write.xlsx(x = ratio_merge, file = "resultados_ratio.xls", sheetName = "res", col.names = TRUE , row.names = FALSE, append = TRUE)
+xlsx::write.xlsx(x = grouped_by_chn, file = "resultados.xls", sheetName = "resultados_ratio", col.names = TRUE , row.names = FALSE, append = TRUE)
+
+xlsx::write.xlsx(x = ratiomerge_container, file = "resultados_ratio.xls", sheetName = "resultados", col.names = TRUE , row.names = FALSE, append = TRUE)
 
 
 xlsx::write.xlsx(x = chn2, file = "chn2.xls", sheetName = "res", col.names = TRUE , row.names = FALSE, append = TRUE)
@@ -36,3 +47,16 @@ xlsx::write.xlsx(x = chn4, file = "chn4.xls", sheetName = "res", col.names = TRU
 xlsx::write.xlsx(x = filelist, file = "file_list.xls", sheetName = "res")
 
 
+
+
+plt<-ggplot(ratiomerge_container, aes(x=Status, y=ratio))+geom_boxplot()+scale_x_discrete(limits = c("05kpa_5min","12kpa_5min","05kpa_15min","12kpa_15min","05kpa_30min","12kpa_30min"))
+
+plt <- ggbetweenstats(
+  data = ratiomerge_container,
+  x = Status,
+  y = ratio,
+  type = "np",
+  results.subtitle = FALSE,
+  pairwise.comparisons = TRUE
+)+scale_x_discrete(limits = c("05kpa_5min","12kpa_5min","05kpa_15min","12kpa_15min","05kpa_30min","12kpa_30min"))
+plt
